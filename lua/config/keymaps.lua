6635fb909c50eb79decbd271598e1e0cf3a99541
Flyingdotschman <dominik.poeschel@hotmail.com>
2024-02-12 01:51:58 +0100

    commit message

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+--
+--Add any additional keymaps hereby
+vim.keymap.set("n", "<space>wl", function()
+  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, opts)
